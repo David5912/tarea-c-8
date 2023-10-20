169dd4feecdb19d62dfc57840d2d51e7272dcd4e
selvin <scanahuis1@miumg.edu.gt>
2023-10-20 17:39:43 -0600

    segundo_commit

---
+++ Cliente.cpp
@@ -0,0 +1,30 @@
+#include "Persona.cpp"
+#include <iostream>
+using namespace std;
+
+class Cliente : Persona {
+//atributos
+private : string nit;
+
+//constructor
+public : 
+Cliente (){
+}
+Cliente(string nom,string ape,string dir,int tel,string n) : Persona(nom,ape,dir,tel){
+	nit = n;
+	
+}
+//metodos
+//set modificar
+void setNit(string n){nit = n;}
+void setNombres(string nom){nombres = nom;}
+void setApellidos(string ape){apellidos = ape;}
+void setDireccion(string dir){direccion = dir;}
+void setTelefono(int tel){telefono = tel;}
+//get (mostrar)
+void mostrar(){
+	cout<<"_____________________"<<endl;
+	cout<<nit<<","<<nombres<<","<<apellidos<<","<<direccion<<","<<telefono<<endl;
+	
+}
+};
